67ffa4cc455a56cd89e26f08158ee79de06bee20
Junior Santos <jnrplay@hotmail.com>
2021-10-31 22:01:31 -0300

    feat: first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const Global = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-size: 10px;
+        box-sizing: border-box;
+    }
+`
